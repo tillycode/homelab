944f6f6b205543809e1612f8010fe8d1bcc5e876
Ziping Sun <me@szp.io>
2024-07-13 15:42:10 +0800

    ci: add setup script (#39)

--- flake/devshell/default.nix
+++ flake/devshell/default.nix
@@ -37,12 +37,46 @@
       devshells.deploy = {
         devshell.motd = "Welcome to CI/CD shell";
         devshell.packages = with pkgs; [
+          openssh
           opentofu
           terragrunt
           jq
           sops
           ssh-to-age
           deploy-rs.deploy-rs
+        ];
+
+        commands = [
+          {
+            package = pkgs.writeShellApplication {
+              name = "setup";
+              text = ''
+                echo "Setting up PATH"
+                echo "$DEVSHELL_DIR/bin" >>"''${GITHUB_PATH:-/dev/stderr}"
+
+                echo "Setting up ssh-agent"
+                declare SSH_AUTH_SOCK SSH_AGENT_PID
+                eval "$(ssh-agent -s)"
+                echo "SSH_AUTH_SOCK=$SSH_AUTH_SOCK" >>"''${GITHUB_ENV:-/dev/stderr}"
+                echo "SSH_AGENT_PID=$SSH_AGENT_PID" >>"''${GITHUB_ENV:-/dev/stderr}"
+
+                echo "Adding private key to ssh-agent"
+                if [[ -n "''${SSH_PRIVATE_KEY:-}" ]]; then
+                  ssh-add - <<<"$SSH_PRIVATE_KEY"
+                else
+                  echo "SSH_PRIVATE_KEY is not set"
+                fi
+
+                echo "Setting up SSH known_hosts"
+                mkdir -p ~/.ssh
+                chmod 700 ~/.ssh
+                cat generated/known_hosts >>~/.ssh/known_hosts
+              '';
+              meta = {
+                description = "setup GitHub Actions environment";
+              };
+            };
+          }
         ];
       };
 
