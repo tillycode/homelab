ed28cc6e37388ca94ca1042b2741a2bcd4da3489
Ziping Sun <me@szp.io>
2024-08-08 02:27:04 +0800

    feat(profiles): add k3s cluster (#74)

--- nixos/hosts/hgh1/default.nix
+++ nixos/hosts/hgh1/default.nix
@@ -1,6 +1,13 @@
-{ suites, modulesPath, ... }:
 {
-  imports = [ (modulesPath + "/profiles/qemu-guest.nix") ] ++ suites.aliyun;
+  suites,
+  profiles,
+  modulesPath,
+  ...
+}:
+{
+  imports = [
+    (modulesPath + "/profiles/qemu-guest.nix")
+  ] ++ suites.aliyun ++ (with profiles; [ services.cluster.k3s-agent ]);
 
   system.stateVersion = "23.11";
 }
---
+++ nixos/profiles/services/cluster/k3s-agent.nix
@@ -0,0 +1,14 @@
+{ config, ... }:
+{
+  services.k3s = {
+    enable = true;
+    role = "agent";
+    serverAddr = "https://hgh2:6443";
+    tokenFile = config.sops.secrets."k3s/bootstrap-token".path;
+    gracefulNodeShutdown.enable = true;
+  };
+
+  sops.secrets."k3s/bootstrap-token" = {
+    sopsFile = config.sops-file.common;
+  };
+}
