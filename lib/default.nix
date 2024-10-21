f476f20990519f6af56f6ca1d36eb7a8051894fb
Ziping Sun <me@szp.io>
2024-10-22 00:49:19 +0800

    feat(data): add data module for nix (#130)

--- lib/default.nix
+++ lib/default.nix
@@ -1,6 +1,35 @@
-{ lib }:
-{
-  data.hosts = lib.pipe ../generated/hosts.json [
+{ lib, inputs }:
+let
+  data = lib.collect lib.isPath (
+    inputs.haumea.lib.load {
+      src = ./data;
+      loader = with inputs.haumea.lib.matchers; [
+        (extension "json" inputs.haumea.lib.loaders.path)
+        (extension "toml" inputs.haumea.lib.loaders.path)
+      ];
+    }
+  );
+  modules = lib.collect lib.isPath (
+    inputs.haumea.lib.load {
+      src = ./modules;
+      loader = with inputs.haumea.lib.matchers; [
+        (extension "nix" inputs.haumea.lib.loaders.path)
+      ];
+    }
+  );
+  result = lib.evalModules {
+    modules = modules ++ [
+      {
+        config = lib.pipe data [
+          # load the data
+          (lib.map (x: if lib.hasSuffix ".json" x then lib.importJSON x else lib.importTOML x))
+          # merge all data
+          lib.mkMerge
+        ];
+      }
+    ];
+  };
+  hosts = lib.pipe ../generated/hosts.json [
     lib.importJSON
     (lib.map (x: {
       name = x.hostname;
@@ -8,4 +37,10 @@
     }))
     lib.listToAttrs
   ];
+in
+{
+  data = result.config // {
+    # for compatibility
+    hosts = hosts;
+  };
 }
