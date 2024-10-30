397a0792b6944461a13ce6908e12794a8750bcd8
Ziping Sun <me@szp.io>
2024-10-31 02:46:33 +0800

    fix(headscale): use latest release instead of beta version

--- pkgs/flake-module.nix
+++ pkgs/flake-module.nix
@@ -2,15 +2,14 @@
 {
   perSystem =
     { pkgs, config, ... }:
-    let
-      sources = pkgs.callPackages (import ./_sources/generated.nix) { };
-    in
+    # let
+    #   sources = pkgs.callPackages (import ./_sources/generated.nix) { };
+    # in
     {
       overlayAttrs = {
-        inherit (config.packages) headscale-beta tailscale-patched;
+        inherit (config.packages) tailscale-patched;
       };
       packages = {
-        headscale-beta = pkgs.callPackage (import ./headscale-beta) { inherit sources; };
         tailscale-patched = pkgs.callPackage (import ./tailscale-patched) { };
       };
     };
