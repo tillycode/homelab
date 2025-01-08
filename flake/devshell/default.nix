e7fc4963026b5b97386239576a2ffb7e8cd4a78a
Ziping Sun <me@szp.io>
2025-01-08 21:17:34 +0800

    refactor(node): reinstall all nodes

--- flake/devshell/default.nix
+++ flake/devshell/default.nix
@@ -6,7 +6,7 @@
 let
   sops-keys = {
     # ziping_sun
-    gpg = [
+    pgp = [
       "8CC5C91F72DB57DA20BD848C6523836CF4992251"
     ];
     kms = [
@@ -15,7 +15,11 @@ let
     ];
   };
   node-keys = {
+    hgh0 = "age1f8jdv6xagt92t44qhnnflkazfe03rry0w6da2eqdv3dwkwppwatqxkgj9w";
     hgh1 = "age1ydjlaersukg2jms5hrjz7z2ja0htlln40uexcmcg2yxzlny8s39qc88wya";
+    hgh2 = "age1q93v8zc62mczmz30wy04n8erlfhhhjc3k7a3vspccjqpvehxadyqgrzrue";
+    sha0 = "age1a46mp4kgk3jdvt520ng9ta45zv0ns0xts9xvjza04t3mpmrfjdqs52s6vh";
+    sin0 = "age1pf03m77v3wpz4qtzc6nrlkvj4pdxffpwtg789g8zjq753hj2ug8qnsnttt";
     desktop = "age1v6lnkm7prm0dpmcdpvn44v50rpfkzsed5uv3znxt4grsd5y6sv5qjru9qq";
   };
   sops_source_creation_rule = {
--- nixos/hosts/hgh0/default.nix
+++ nixos/hosts/hgh0/default.nix
@@ -1,27 +1,22 @@
 {
-  suites,
-  profiles,
-  modulesPath,
   ...
 }:
 {
-  imports =
-    [ (modulesPath + "/profiles/qemu-guest.nix") ]
-    ++ suites.aliyun
-    ++ (with profiles; [
-      services.cluster.k3s-agent
-      services.cluster.k3s-common
-      services.cluster.k3s-ingress
-      services.networking.nginx
-      services.networking.headscale
-      services.networking.headscale-nginx
-    ]);
+  # imports =
+  #   [ (modulesPath + "/profiles/qemu-guest.nix") ]
+  #   ++ suites.aliyun
+  #   ++ (with profiles; [
+  #     services.cluster.k3s-agent
+  #     services.cluster.k3s-common
+  #     services.cluster.k3s-ingress
+  #     services.networking.nginx
+  #     services.networking.headscale
+  #     services.networking.headscale-nginx
+  #   ]);
 
   networking.nat = {
     enable = true;
     internalInterfaces = [ "ens5" ];
     externalInterface = "ens5";
   };
-
-  system.stateVersion = "23.11";
 }
