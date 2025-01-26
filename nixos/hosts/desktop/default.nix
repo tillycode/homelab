bb47e1a34baae2fd45df775b7183be6eff06aa06
Ziping Sun <me@szp.io>
2025-01-27 01:17:19 +0800

    feat(desktop): enable GPU passthrough (#195)

--- nixos/hosts/desktop/default.nix
+++ nixos/hosts/desktop/default.nix
@@ -7,8 +7,28 @@
     extraPackages = with pkgs; [ intel-media-driver ];
   };
 
-  services.xserver.videoDrivers = [ "nvidia" ];
+  # services.xserver.videoDrivers = [ "nvidia" ];
+  # hardware.nvidia.open = false;
+  # hardware.nvidia-container-toolkit.enable = true;
 
-  hardware.nvidia.open = false;
-  hardware.nvidia-container-toolkit.enable = true;
+  boot.kernelParams = [
+    "intel_iommu=on"
+    "iommu=pt"
+  ];
+  boot.kernelModules = [
+    "vfio"
+    "vfio_iommu_type1"
+    "vfio_pci"
+    "vfio_virqfd"
+    "xhci_pci"
+  ];
+  boot.extraModprobeConfig = ''
+    options vfio-pci ids=10de:1e84,10de:10f8,10de:1ad9
+  '';
+  boot.initrd.preDeviceCommands = ''
+    DEVS="01:00.2"
+    for DEV in $DEVS; do
+        echo none > "/sys/bus/pci/devices/0000:$DEV/driver_override"
+    done
+  '';
 }
