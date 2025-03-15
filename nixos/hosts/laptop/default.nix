35124d432f0455cc4202b855e03b9dcae926dbd7
Ziping Sun <me@szp.io>
2025-03-15 13:48:03 +0800

    feat(laptop): add steam loop fs & add power profiles (#234)

--- nixos/hosts/laptop/default.nix
+++ nixos/hosts/laptop/default.nix
@@ -2,4 +2,25 @@
   imports = [
     ./hardware.nix
   ];
+  boot.kernelParams = [ "resume_offset=533760" ];
+  boot.resumeDevice = "/dev/disk/by-partlabel/disk-vda-root";
+
+  # TODO: add personal profiles to avoid the hard code
+  # sudo mkfs.vfat -C -F 32 civ6mods.img 2097152
+  # lsblk
+  # sudo losetup /dev/loop28 civ6mods.img
+  # sudo mount -o uid=<user>,gid=users,fmask=113,dmask=002,iocharset=iso8859-1 /dev/loop28 "/home/<username>/.local/share/Steam/steamapps/workshop/content/"
+  fileSystems."/home/sun/.local/share/Steam/steamapps/workshop/content" = {
+    device = "/home/sun/.local/share/Steam/steamapps/workshop/content.img";
+    fsType = "vfat";
+    options = [
+      "loop"
+      "uid=sun"
+      "gid=users"
+      "fmask=133"
+      "dmask=022"
+      "iocharset=iso8859-1"
+      "nofail"
+    ];
+  };
 }
---
+++ nixos/profiles/services/power-profiles-daemon.nix
@@ -0,0 +1,4 @@
+{
+  # TODO: migrate to TuneD https://github.com/NixOS/nixpkgs/pull/357480
+  services.power-profiles-daemon.enable = true;
+}
