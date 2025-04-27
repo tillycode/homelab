c2eb980712f6507f8168b1090bde380b0643556a
Ziping Sun <me@szp.io>
2025-04-27 22:10:36 +0800

    feat(service): deploy firefly-iii

---
+++ nixos/profiles/services/firefly-iii.nix
@@ -0,0 +1,49 @@
+{ config, ... }:
+let
+  domain = config.domains.firefly-iii;
+  cfg = config.services.firefly-iii;
+in
+{
+  ## ---------------------------------------------------------------------------
+  ## CONFIGURATION
+  ## ---------------------------------------------------------------------------
+  services.firefly-iii = {
+    enable = true;
+    enableNginx = true;
+    virtualHost = domain;
+
+    settings = {
+      SITE_OWNER = "me@szp.io";
+      APP_KEY_FILE = config.sops.secrets."firefly-iii/appKey".path;
+      TZ = "Asia/Shanghai";
+      TRUSTED_PROXIES = "**";
+    };
+  };
+
+  ## ---------------------------------------------------------------------------
+  ## SECRETS
+  ## ---------------------------------------------------------------------------
+  sops.secrets."firefly-iii/appKey" = {
+    owner = cfg.user;
+  };
+
+  ## ---------------------------------------------------------------------------
+  ## PERSISTENCE
+  ## ---------------------------------------------------------------------------
+  environment.persistence.default.directories = [
+    {
+      directory = cfg.dataDir;
+      mode = "0700";
+      user = cfg.user;
+      group = cfg.group;
+    }
+  ];
+
+  ## ---------------------------------------------------------------------------
+  ## INGRESS
+  ## ---------------------------------------------------------------------------
+  services.nginx.virtualHosts.${domain} = {
+    forceSSL = true;
+    enableACME = true;
+  };
+}
